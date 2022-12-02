a10d5662aa20e50c44a06dd5f0bcbfb2009eae9a
fan-droide <jose.gilpanal@telecom-paris.fr>
2022-12-02 14:28:14 +0100

    use getContainedResourceUrlAll for detail page

--- src/js/detailview.js
+++ src/js/detailview.js
@@ -1,56 +1,50 @@
 import {
     getSolidDataset,
-    getThing,
-    getThingAll,
-    getUrl
-} from "@inrupt/solid-client";
-import { fetch } from "@inrupt/solid-client-authn-browser";
-import { RDF } from "@inrupt/vocab-common-rdf";
+    getContainedResourceUrlAll    
+} from "@inrupt/solid-client"
+import { fetch } from "@inrupt/solid-client-authn-browser"
+import { RDF } from "@inrupt/vocab-common-rdf"
 
 const SOLID_IDENTITY_PROVIDER = "https://warm-inlet-30289.herokuapp.com"
 
 async function init() {
     let queryString = window.location.search
     queryString = queryString.split('path=')[1]
-    let myReadingList;
+    let myReadingList
 
     try {
 
-        myReadingList = await getSolidDataset(SOLID_IDENTITY_PROVIDER + "/" + queryString + "/", { fetch: fetch });
-        let items = getThingAll(myReadingList)
-        console.log(myReadingList)
+        myReadingList = await getSolidDataset(SOLID_IDENTITY_PROVIDER + "/" + queryString + "/", { fetch: fetch })
+
+        let items = getContainedResourceUrlAll(myReadingList, { fetch: fetch })
 
         for (let i = 0; i < items.length; i++) {
 
-            let item = getUrl(items[i], RDF.type)
-
+            let item = items[i]
+            console.log(item)
             if (item !== null) {
-                if (item === "http://www.w3.org/ns/ldp#Resource") {
 
-                    let cosica = getThing(myReadingList, items[i].url)
-                    if (isAudioType(cosica.url)) {
-                        paintPlayer(cosica.url)
-                    }
+                if (isAudioType(item)) {
+                    paintPlayer(item)
                 }
             }
         }
 
-
     } catch (error) {
         if (typeof error.statusCode === "number" && error.statusCode === 404) {
-            console.error(error);
+            console.error(error)
         } else {
-            console.error(error.message);
+            console.error(error.message)
         }
     }
 }
 
 function isAudioType(file) {
-    return /\.(mp3|m4a)$/i.test(file);
+    return /\.(mp3|m4a)$/i.test(file)
 }
 
 function paintPlayer(url) {
-    document.getElementById('song').innerHTML = '<audio controls="controls" src="' + url + '">';
+    document.getElementById('song').innerHTML = '<audio controls="controls" src="' + url + '">'
 
 }
 init() 
